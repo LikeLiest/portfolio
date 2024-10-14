f7a8993b6b170f11e2246d35152ca894161e707b
Ivan <rasimzakirov9@gmail.com>
2024-10-15 00:21:06 +0300

    header is finished

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -3,10 +3,12 @@ import style from "./Header.module.scss";
 export const Header = () => {
   return (
     <header className={style.header}>
-      <h1>Hi, my name is Rasim</h1>
-      <h2>a fullstack developer</h2>
-      <h3>with passion for learning and creating.</h3>
-      <button>????</button>
+      <div className={style.header_wrapper}>
+        <h1>Hi, my name is <em>Rasim</em></h1>
+        <h2>a fullstack developer</h2>
+        <h3>with passion for learning and creating.</h3>
+        <button>Download CV</button>
+      </div>
     </header>
   );
 };
