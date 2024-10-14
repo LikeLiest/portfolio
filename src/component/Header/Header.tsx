7b689289defbf91f8968c9200af3e370a7fab78e
Ivan <rasimzakirov9@gmail.com>
2024-10-15 00:21:31 +0300

    header is finished

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -4,7 +4,9 @@ export const Header = () => {
   return (
     <header className={style.header}>
       <div className={style.header_wrapper}>
-        <h1>Hi, my name is <em>Rasim</em></h1>
+        <h1>
+          Hi, my name is <em>Rasim</em>
+        </h1>
         <h2>a fullstack developer</h2>
         <h3>with passion for learning and creating.</h3>
         <button>Download CV</button>
